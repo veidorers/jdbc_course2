367b25f058611e874ac4ce72bf7a67047976fd0f
Sergey Fomenko <artem.work749@gmail.com>
2023-12-12 16:59:36 +0300

    Connection to the database

--- src/main/java/com/example/JdbcRunner.java
+++ src/main/java/com/example/JdbcRunner.java
@@ -1,7 +1,13 @@
 package com.example;
 
-public class JdbcRunner {
-    public static void main(String[] args) {
+import com.example.util.ConnectionManager;
 
+import java.sql.SQLException;
+
+public class JdbcRunner {
+    public static void main(String[] args) throws SQLException {
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 }
